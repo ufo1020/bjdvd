da8aaf6b5445412b4a10a85cdb438d6fd8156c17
Mason Zhang <Mason.Xin.Zhang@gmail.com>
2014-11-27 17:13:10 +1100

    Working on the go

--- userdata.h
+++ userdata.h
@@ -7,16 +7,19 @@ class userData
 {
 public:
     userData();
-    QByteArray to_utf8_bytes(QString& string){
-            QString str = QString::fromUtf8("");
-            for (QString::iterator it = str.begin(); it != str.end(); ++it)
-            {
-                std::cout<< std::hex << it->unicode()<< std::endl;
-            }
+    ~userData();
+    QByteArray to_utf8_bytes(QString& string);
+
+    void set_username(QString& username)
+    {
+        m_username = username;
     }
 
-    void set_username(QString&);
-    void set_password(QString&);
+    void set_password(QString& password)
+    {
+        m_password = password;
+    }
+
 private:
     QString m_username;
     QString m_password;
