3b02bffa7c35e85268230e98a1b420cd3990e1ee
Mason Zhang <Mason.Xin.Zhang@gmail.com>
2014-12-03 18:21:06 +1100

    Added utf8 bytes conversion

--- userdata.cpp
+++ userdata.cpp
@@ -14,10 +14,34 @@ userData::~userData()
 
 QByteArray userData::to_utf8_bytes(QString& string)
 {
+    QByteArray bytes;
     for(auto it: string)
     {
         QString str;
-        qDebug()<<"unicode "<<str.setNum(it.unicode(), 16);
+        uint hex_code = it.unicode();
+
+        if (hex_code < ASCII_CODE) {
+            bytes.append(str.setNum(hex_code, 16));
+        }
+        else {
+            uint bytes_sequence = 0;
+            while (hex_code) {
+                // it is the 1st byte
+                if (hex_code >> BITS_PER_UTF8_BYTE == 0) {
+                    uint lower_header = UTF_BYTE_HEADER >> bytes_sequence;
+                    uint higher_header = ~((1U << (BITS_PER_BYTE - bytes_sequence)) -1U);
+                    uint byte = hex_code | lower_header | higher_header;
+                    bytes.insert(0, str.setNum(byte, 16));
+                }
+                else {
+                    // the lowest 6 bits
+                    uint byte = hex_code | (1U << BITS_PER_UTF8_BYTE - 1U);
+                    bytes.insert(0, str.setNum(byte, 16));
+                }
+                hex_code = hex_code >> BITS_PER_UTF8_BYTE;
+                ++bytes_sequence;
+            }
+        }
     }
 
     return "a";
@@ -27,7 +51,7 @@ uint userData::get_mask(int bytes)
 {
     // uft-8 has 1-4 bytes
     Q_ASSERT(bytes >= 1 && bytes <= 4);
-    uint bits = BITS_PER_BYTE * bytes;
+    uint bits = BITS_PER_BYTE * bytes - 1;
     constexpr uint limit = BITS_PER_BYTE * sizeof(uint);
     constexpr uint allBits = ~0U;
     uint mask = 0;
--- userdata.h
+++ userdata.h
@@ -22,7 +22,26 @@ public:
 
 private:
     static const uint BITS_PER_BYTE = 8U;
+    static const uint BITS_PER_UTF8_BYTE = 6U;
+
+    static const uint ASCII_CODE = 1U << 7U;
+    static const uint UTF_BYTE_HEADER = 1U << 7U;
+
+    // UTF-8 1 byte for U+000 - U+007F (7 bits)
+    static const uint BITS_OF_CODE_POINT_ONE_BYTE = 1U << 6U;
+    // UTF-8 2 bytes for U+0080 - U+07FF (11 bits)
+    static const uint BITS_OF_CODE_POINT_TWO_BYTES = 1U << 10U;
+    // UTF-8 3 bytes for U+0800 - U+FFFF (16 bits)
+    static const uint BITS_OF_CODE_POINT_THREE_BYTES = 1U << 15U;
+    // UTF-8 4 bytes for U+10000 - U+1FFFFF (21 bits)
+    static const uint BITS_OF_CODE_POINT_FOUR_BYTES = 1U << 20U;
+
+    // 1 byte mask: 7F, 2 bytes mask: 7FF, 3 bytes mask:FFFF,
+    // 4 bytes mask: 1FFFFF
     uint get_mask(int bytes);
+
+    //
+    uint get_bytes(uint unicode);
 
     QString m_username;
     QString m_password;
